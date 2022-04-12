f6ef2a8ef05adb3c09997b85263cd0379ff02bcf
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2022-04-12 10:36:18 +0000

    support gopls

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -4,10 +4,17 @@ local M = {}
 M.setup = function(on_attach)
   null_ls.setup({
       sources = {
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.code_actions.eslint,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.code_actions.gitsigns,
+        -- typescript
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.formatting.prettier,
+
+
+        -- golang
+        null_ls.builtins.diagnostics.golangci_lint
+
+        -- other
+        -- null_ls.builtins.code_actions.refactoring
       },
       on_attach = on_attach,
   })
