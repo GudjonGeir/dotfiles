6c2e217eb66108630ebab261d7cf9a8d49838697
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2022-06-03 09:49:44 +0000

    update null-ls eslint_d and prettierd

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -5,9 +5,9 @@ M.setup = function(on_attach)
   null_ls.setup({
       sources = {
         -- typescript
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.prettierd,
 
 
         -- golang
