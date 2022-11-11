ba2b7696918288a9853382c2d4e472d5cfc5843c
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2022-11-11 09:37:40 +0000

    sql formatter

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -3,6 +3,7 @@ local null_ls = require("null-ls")
 local M = {}
 M.setup = function(on_attach)
   null_ls.setup({
+      debug = true,
       sources = {
         -- typescript
         null_ls.builtins.diagnostics.eslint_d,
@@ -11,7 +12,15 @@ M.setup = function(on_attach)
 
 
         -- golang
-        null_ls.builtins.diagnostics.golangci_lint
+        null_ls.builtins.diagnostics.golangci_lint,
+
+        -- postgres
+        null_ls.builtins.formatting.sqlfluff.with({
+          extra_args = {"--dialect", "postgres"}
+        }),
+        null_ls.builtins.diagnostics.sqlfluff.with({
+          extra_args = {"--dialect", "postgres"}
+        })
 
         -- other
         -- null_ls.builtins.code_actions.refactoring
