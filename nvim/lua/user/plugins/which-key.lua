336a9f3ccee037b9956dde632a1adb8fa060d0c5
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2025-05-16 08:44:21 +0000

    update which key syntax

--- nvim/lua/user/plugins/which-key.lua
+++ nvim/lua/user/plugins/which-key.lua
@@ -5,56 +5,26 @@ return {
 	config = function()
 		require("which-key").setup()
 
-		require("which-key").register({
-			["<leader>"] = {
-				b = {
-					name = "+buffer",
-				},
-				c = {
-					name = "+comments",
-				},
-				f = {
-					name = "+files",
-					e = {
-						name = "+environment",
-					},
-					t = {
-						name = "+tests",
-					},
-				},
-				g = {
-					name = "+git/version-control",
-					t = {
-						name = "+toggles",
-					},
-				},
-				t = {
-					name = "+tests",
-				},
-				T = {
-					name = "+toggles",
-				},
-				s = {
-					name = "+search",
-				},
-				w = {
-					name = "+window",
-				},
-			},
-			["<localleader>"] = {
-				d = {
-					name = "+diagnostics",
-				},
-				g = {
-					name = "+goto",
-				},
-				s = {
-					name = "+search",
-				},
-				r = {
-					name = "+refactor",
-				},
-			},
+		require("which-key").add({
+			{ "<leader>b", group = "+buffer" },
+			{ "<leader>c", group = "+comments" },
+
+			{ "<leader>f", group = "+files" },
+			{ "<leader>fe", group = "+environment" },
+			{ "<leader>ft", group = "+toggles" },
+
+			{ "<leader>g", group = "+git/version-control" },
+			{ "<leader>gt", group = "+toggles" },
+
+			{ "<leader>t", group = "+tests" },
+			{ "<leader>T", group = "+toggles" },
+			{ "<leader>s", group = "+search" },
+			{ "<leader>w", group = "+window" },
+
+			{ "<localleader>d", group = "+diagnostics" },
+			{ "<localleader>g", group = "+goto" },
+			{ "<localleader>s", group = "+search" },
+			{ "<localleader>r", group = "+refactor" },
 		})
 	end,
 }
